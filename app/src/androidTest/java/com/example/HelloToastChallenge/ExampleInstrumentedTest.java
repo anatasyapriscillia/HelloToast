ca65334c6d5317d16e4766ecbdf0cb4e91a2d5ad
anatasyapriscillia <manoppoanatasya@gmail.com>
2019-01-24 18:35:29 +0700

    Hasil Challenge Week 1

--- app/src/androidTest/java/com/example/HelloToastChallenge/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/HelloToastChallenge/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.hellotoast;
+package com.example.HelloToastChallenge;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.hellotoast", appContext.getPackageName());
+        assertEquals("com.example.HelloToastChallenge", appContext.getPackageName());
     }
 }
